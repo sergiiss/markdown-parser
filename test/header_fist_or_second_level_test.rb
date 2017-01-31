1d785625664d42df59968316b393b061ce6227a8
Yuri Kaspervich <ykas.gg@gmail.com>
2017-01-31 20:33:11 +0300

    Add markdown_parser exe

--- test/header_fist_or_second_level_test.rb
+++ test/header_fist_or_second_level_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 
 class TestHeaderFistOrSecondLevel < Minitest::Test
   def test_get_the_header_level_2
-    headers = HeaderFistOrSecondLevel.new("Заголовок 2 уровня\n------------------\n").convert_headers_first_or_second_level_in_html
+    header = HeaderFistOrSecondLevel.new("Заголовок 2 уровня\n------------------\n")
     
-    assert_equal "<h2>Заголовок 2 уровня</h2>\n", headers
+    result = header.convert_headers_first_or_second_level_in_html
+    
+    assert_equal "<h2>Заголовок 2 уровня</h2>\n", result
   end  
 end
--- test/header_test.rb
+++ test/header_test.rb
@@ -2,8 +2,10 @@ require 'test_helper'
 
 class TestHeader < Minitest::Test
   def test_get_the_header_level_3
-    header = Header.new("### Заголовок 3 уровня\n").convert_headers_in_html
+    header = Header.new("### Заголовок 3 уровня\n")
     
-    assert_equal "<h3>Заголовок 3 уровня</h3>\n", header
+    result = header.convert_headers_in_html
+    
+    assert_equal "<h3>Заголовок 3 уровня</h3>\n", result
   end  
 end
